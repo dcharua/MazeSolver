81f05d4f4574594f646f23002b63cbc459dddcc6
dcharua <danielcharua@hotmail.com>
2018-09-25 14:36:54 -0500

    return path

--- maze.py
+++ maze.py
@@ -1,6 +1,9 @@
 import numpy as np
+import sys
+
 class Maze:
     def __init__(self, filename):
+        self.path = ""
         tmp = []
         lines = list(open(filename))
         a, b = lines[0].rstrip().split(' ')
@@ -13,49 +16,52 @@ class Maze:
         for line in reversed(lines[3:]):
             tmp.append(list(map(int, line.rstrip())))
         self.maze = np.array(tmp)
-        self.solve(self.start_point)
+        self.solve(self.start_point, self.path)
         c, r = self.start_point
         self.maze[r][c] = 3
         c, r = self.end_point
         self.maze[r][c] = 4
         self.printMaze()
 
+
     def printMaze(self):
         print('\nCol: %d, Rows: %d' % (self.col, self.row))
         print('start_point: %s marked with 3' % (self.start_point,))
         print('end_point: %s marked with 4' % (self.end_point,))
+        print('Path taken: %s ' % (self.path[::-1]))
         print (self.maze)
 
-    def solve(self, coordinates):
+    def solve(self, coordinates, path):
         c, r = coordinates
         if coordinates == self.end_point:
-            print("Maze solved")
-            print(coordinates)
             return True
         elif self.maze[r][c] == 1:
-            print ('wall at %d,%d' % (c, r))
             return False
         elif self.maze[r][c] == 2:
-            print ('visited at %d,%d' % (c, r))
             return False
-        print ('visiting %d,%d' % (c, r))
         self.maze[r][c] = 2
 
-        if (c < self.col-1 and self.solve((c+1, r))):
-            print ("R")
+        if (c < self.col-1 and self.solve((c+1, r), self.path)):
+            self.path +="R"
             return True
-        elif (r > 0 and self.solve((c, r-1))):
-            print ("U")
+        elif (r > 0 and self.solve((c, r-1), self.path)):
+            self.path +="D"
             return True
-        elif (c > 0 and self.solve((c-1, r))):
-            print ("L")
+        elif (c > 0 and self.solve((c-1, r), self.path)):
+            self.path +="L"
             return True
-        elif(r < self.row-1 and self.solve((c, r+1))):
-            print ("D")
+        elif(r < self.row-1 and self.solve((c, r+1), self.path)):
+            self.path +="U"
             return True
         return False
+
+    def getPath(self):
+        return self.path[::-1]
 
 
 
 if __name__ == "__main__":
-    maze = Maze("maze2.txt")
+    maze = Maze(sys.argv[1])
+    sys.stdout.write( maze.getPath())
+    sys.stdout.flush()
+    sys.exit(0)
